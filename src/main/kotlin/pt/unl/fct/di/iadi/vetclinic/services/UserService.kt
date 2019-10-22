ad739e750390eddb8c10cdefb9a8406ec2f16b96
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-10-22 04:37:54 +0100

    Clean up

--- src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/services/UserService.kt
+++ src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/services/UserService.kt
@@ -2,25 +2,25 @@ package pt.unl.fct.di.iadi.vetclinic.services
 
 import org.springframework.stereotype.Service
 import pt.unl.fct.di.iadi.vetclinic.model.AppointmentDAO
-//import pt.unl.fct.di.iadi.vetclinic.model.UserDAO
-//import pt.unl.fct.di.iadi.vetclinic.model.UserRepository
-//
-//@Service
-//class UserService(val user: UserRepository) {
-//
-//    //fun getAllAppointment():List<AppointmentDAO> = appointment.findAll().toList()
-//
-//    fun addNewUser(client: UserDAO) {
-//        user.save(client)
-//    }
-//
-//    fun getOneUser(id:Long) =
-//            user.findById(id).orElseThrow { NotFoundException("There is no client with Id $id") }
-//
-//
-//    fun deleteUser(id:Long) {
-//        val client: UserDAO = getOneUser(id)
-//
-//        user.delete(client)
-//    }
-//}
+import pt.unl.fct.di.iadi.vetclinic.model.UserDAO
+import pt.unl.fct.di.iadi.vetclinic.model.UserRepository
+
+@Service
+class UserService(val user: UserRepository) {
+
+    fun getAllUser():List<UserDAO> = user.findAll().toList()
+
+  fun addNewUser(client: UserDAO) {
+       user.save(client)
+   }
+
+   fun getOneUser(id:Long) =
+            user.findById(id).orElseThrow { NotFoundException("There is no client with Id $id") }
+
+
+  fun deleteUser(id:Long) {
+        val client: UserDAO = getOneUser(id)
+
+       user.delete(client)
+   }
+}
