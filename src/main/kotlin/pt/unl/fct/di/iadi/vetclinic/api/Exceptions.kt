70b78fc3ddf24d81f376678da21daca02b012130
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-10-22 17:58:20 +0100

    Excepções

--- src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/api/Exceptions.kt
+++ src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/api/Exceptions.kt
@@ -5,3 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 class HTTPNotFoundException(s: String) : Exception(s)
+
+class NotFoundException(s:String): RuntimeException(s)
+
+class PreconditionFailedException(s:String) : RuntimeException(s)
