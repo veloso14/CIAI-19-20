9960bc27ab132d06e26126e18f27609ca964e96e
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-10-26 18:13:59 +0100

    Admin repo

---
+++ src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/api/AdminController.kt
@@ -0,0 +1,48 @@
+package pt.unl.fct.di.iadi.vetclinic.api
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import pt.unl.fct.di.iadi.vetclinic.services.AdminService
+import pt.unl.fct.di.iadi.vetclinic.services.UserService
+
+
+@Api(value = "VetClinic Management System - Admin API",
+        description = "Management operations of ADMIN in the IADI 2019 Pet Clinic")
+@RestController
+@RequestMapping("/user/admin")
+
+class AdminController(val admin: AdminService) {
+    @ApiOperation(value = "View a list of registered users", response = List::class)
+    @ApiResponses(value = [
+        ApiResponse(code = 200, message = "Successfully retrieved user list"),
+        ApiResponse(code = 401, message = "You are not authorized to view this resource"),
+        ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden")
+    ])
+    @GetMapping("/employees")
+    fun getAllUsers() = admin.getAllEmployees().map { UserDTO(it) }
+
+    @GetMapping("/pets")
+    fun getAllPets() = admin.getAllPets().map { PetDTO(it) }
+
+    @GetMapping("/clients")
+    fun getAllClients() = admin.getAllClients().map { UserDTO(it) }
+
+    @GetMapping("/appointments")
+    fun getAllAppointments() = admin.getAllAppointments().map { AppointmentDTO(it) }
+
+    @GetMapping("/appointments/{id}")
+    fun getAppointmentsByVetId(@PathVariable id: String) = handle404 { admin.getAppointmentsByVetId(id).map { AppointmentDTO(it) } }
+
+    @GetMapping("/employees/{id}")
+    fun fireEmployee(@PathVariable id: String) = handle404 { admin.fireEmployee(id) }
+
+    @GetMapping("/schedule/{id}")
+    fun setVetSchedule(@PathVariable id: String) = handle404 { admin.setVetSchedule(id) }
+
+}
---
+++ src/main/kotlin/pt/unl/fct/di/iadi/vetclinic/services/AdminService.kt
@@ -0,0 +1,102 @@
+package pt.unl.fct.di.iadi.vetclinic.services
+
+import org.springframework.stereotype.Service
+import pt.unl.fct.di.iadi.vetclinic.model.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Month
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+@Service
+class AdminService(
+        val pets: PetRepository,
+        val clients: ClientRepository,
+        val appointments: AppointmentRepository,
+        val users: UserRepository
+     //   val schedules: VetScheduleRepository
+) {
+    // val logger = LoggerFactory.getLogger(AdminService::class.java)
+
+    fun getAllEmployees(): List<UserDAO> = users.findAll().toList()
+
+    fun getAllPets(): List<PetDAO> = pets.findAll().toList()
+
+    fun getAllClients(): List<UserDAO> = clients.findAll().toList()
+
+    fun getAllAppointments(): List<AppointmentDAO> = appointments.findAll().toList();
+
+    fun findEmployee(id: String): UserDAO = users.findById(id).orElseThrow { NotFoundException("There is no user with Id $id") }
+
+    // if employee is admin remove account; if employee is vet freeze account
+    fun fireEmployee(id:String) {
+        val user = findEmployee(id)
+        if (user is AdminDAO) {
+            users.deleteById(id)
+        } else if (user is VetDAO) {
+            user.updateFrozen(true)
+        }
+    }
+
+    // returns the list of appointments of a single Vet by giving his unique Id
+    fun getAppointmentsByVetId(id: String): MutableList<AppointmentDAO> {
+        val vet = findEmployee(id)
+        var appointments = mutableListOf<AppointmentDAO>()
+        if (vet is VetDAO) {
+            appointments = vet.appointments
+        }
+        return appointments
+    }
+
+    fun setVetSchedule(id: String) {
+        val vet = findEmployee(id)
+        if (vet is VetDAO) {
+            // check first if said vet already has a schedule created and throw exception if that's the case TODO
+            // val existingSchedule = schedules.findById(...)
+            // if (existingSchedule isPresent) throw Exception
+            val schedule = createSchedule(vet)
+            val vetSchedule = VetScheduleDAO(vet, schedule)
+           // schedules.save(vetSchedule)
+
+        } else {
+            throw NotFoundException("Vet with given Id:${id} doesn't exist.")
+        }
+    }
+
+    fun createSchedule(vet: VetDAO): MutableMap<Int, MutableList<ShiftDAO>> {
+        val newSchedule = mutableMapOf<Int, MutableList<ShiftDAO>>()
+        for (x in 0..29) {
+            val newShiftList = createShiftList(vet)
+            newSchedule[x] = newShiftList
+        }
+        return newSchedule
+    }
+
+    // lista com 16 posições correspondendo aos slots que são possiveis marcar uma consulta.
+    // posiçao 0: 9:00
+    // posiçao 15: 17:00
+    // com incrementos de 30 minutos
+    // cada shift na lista corresponde a um possivel slot de appointment que e possivel ver se esta
+    // disponivel atraves de um booleano
+    // temos que assumir que cada posiçao corresponde a um slot de 30 min
+    fun createShiftList(vet: VetDAO): MutableList<ShiftDAO> {
+        val list = mutableListOf<ShiftDAO>()
+        for (x in 0..15) {
+            val newShift = ShiftDAO(vet)
+            list.add(x, newShift)
+        }
+        return list
+    }
+
+    // como somar de 30 em 30 minutos
+//    fun createShift(x: Int) {
+//        val year = 2019
+//        val month = Month.OCTOBER
+//        val start = LocalDateTime.of(year, month, x, 9, 30)
+//        val end = start.plusMinutes(30)
+//    }
+
+}
+
+
