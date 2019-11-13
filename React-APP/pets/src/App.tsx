6d9c0f505971abc5b789618b67149e45783a9d79
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-11-13 06:16:47 +0000

    Mostra sem ir buscar ao servidor

--- React-APP/pets/src/App.tsx
+++ React-APP/pets/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Pet {
+  name:String,
+  species:String
 }
+
+const PetList = (  props:{ pets:Pet[]} ) => {
+  let petlist = props.pets.map(pet => <li> {pet.name} is {pet.species}</li>);
+  return <ul>{petlist}</ul>
+}
+
+const App: React.FC = () =>
+    <PetList pets={[{name:"grilo" ,species:"Cão"}]}/>;
 
 export default App;
