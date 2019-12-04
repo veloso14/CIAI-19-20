b918ab22662b113fc94e46e37d800e611622df47
bmamaral <bm.amaral@campus.fct.unl.pt>
2019-12-04 00:21:42 +0000

    home page + 404

---
+++ React-APP/pets/src/components/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div className="container text-center">Page not Found</div>
+    );
+}
+
+export default NoMatch;
--- React-APP/pets/src/components/SignIn.tsx
+++ React-APP/pets/src/components/SignIn.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent, FormEvent, useState} from "react";
 import {connect, Provider} from "react-redux";
 import App, {GlobalState} from "../App";
 import {requestSignIn, signOut} from "../actions/SignInAction";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import thunk from 'redux-thunk';
 import PetList from "./PetList";
 import PetDetails from "./PetDetails";
@@ -12,34 +12,42 @@ import AppointmentDetails from "./AppointmentDetails";
 import AddAppointmentForm from "./AddAppointmentForm";
 import {applyMiddleware, createStore} from "redux";
 import AdminList from "./AdminList";
+import Home from "./Home";
 import AdminDetails from "./AdminDetails";
 import SignOutForm from "./SignOut";
 import ChangePassword from "./ChangePassword";
 import VetList from "./VetList";
 import Container from "react-bootstrap/Container";
+import NoMatch from "./NoMatch";
 
-export interface SignInState { isSignedIn: boolean }
+export interface SignInState {
+    isSignedIn: boolean
+}
 
 const ProtoSignInForm = (
-    props:{
-        isSignedIn:boolean,
-        performSignIn:(username:string, password:string)=>void,
-        performSignOut:()=>void
+    props: {
+        isSignedIn: boolean,
+        performSignIn: (username: string, password: string) => void,
+        performSignOut: () => void
     }) => {
 
-    const [ username, setUsername ] = useState("");
-    const [ password, setPassword ] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    let submitHandler = (e:FormEvent<HTMLFormElement>) => {
+    let submitHandler = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         props.performSignIn(username, password);
         setUsername("");
         setPassword("")
     };
 
-    let usernameChangeHandler = (e:ChangeEvent<HTMLInputElement>) => { setUsername(e.target.value) };
+    let usernameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value)
+    };
 
-    let passwordChangeHandler = (e:ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value) };
+    let passwordChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    };
 
     let signInForm =
         (
@@ -51,16 +59,18 @@ const ProtoSignInForm = (
 
 
                 <form onSubmit={submitHandler}>
-            <div className="form-group">
-                <label>Username: <input className="form-control" type="text" value={username} onChange={usernameChangeHandler}/></label>
-            </div>
-            <div className="form-group">
-                <label>Password: <input className="form-control" type="password" value={password} onChange={passwordChangeHandler}/></label>
-            </div>
-            <button>Sign In</button>
+                    <div className="form-group">
+                        <label>Username: <input className="form-control" type="text" value={username}
+                                                onChange={usernameChangeHandler}/></label>
+                    </div>
+                    <div className="form-group">
+                        <label>Password: <input className="form-control" type="password" value={password}
+                                                onChange={passwordChangeHandler}/></label>
+                    </div>
+                    <button>Sign In</button>
                     <br/> <br/>
                     <button>Registar</button>
-        </form>
+                </form>
             </Container>);
 
 
@@ -79,15 +89,19 @@ const ProtoSignInForm = (
         <Provider store={store}>
             <NavigationBar/>
             <Router>
-                <Route path="/pet/" exact component={PetList} />
-                <Route path="/pet/:id" component={PetDetails} />
-                <Route path="/appointment/:id" component={AppointmentDetails} />
-                <Route path="/appointment/" exact component={AddAppointmentForm} />
-                <Route path="/admin/" exact component={AdminList} />
-                <Route path="/admin/:id" component={AdminDetails} />
-                <Route path="/profile/" component={ChangePassword} />
-                <Route path="/vet/" exact component={VetList} />
-                <Route path="/logout/" exact component ={SignOutForm} />
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/pet/" exact component={PetList}/>
+                    <Route path="/pet/:id" component={PetDetails}/>
+                    <Route path="/appointment/:id" component={AppointmentDetails}/>
+                    <Route path="/appointment/" exact component={AddAppointmentForm}/>
+                    <Route path="/admin/" exact component={AdminList}/>
+                    <Route path="/admin/:id" component={AdminDetails}/>
+                    <Route path="/profile/" component={ChangePassword}/>
+                    <Route path="/vet/" exact component={VetList}/>
+                    <Route path="/logout/" exact component={SignOutForm}/>
+                    <Route component={NoMatch}/>
+                </Switch>
             </Router>
             <Page/>
         </Provider>
@@ -97,14 +111,16 @@ const ProtoSignInForm = (
     return (<> {props.isSignedIn ? signOutForm : signInForm} </>);
     // add a message space for alerts (you were signed out, expired session)
 };
-const mapStateToProps = (state:GlobalState) => ({isSignedIn:state.signIn.isSignedIn});
+const mapStateToProps = (state: GlobalState) => ({isSignedIn: state.signIn.isSignedIn});
 const mapDispatchToProps =
-    (dispatch:any) =>
+    (dispatch: any) =>
         ({
-            performSignIn: (username:string, password:string) => { dispatch(requestSignIn(username,password))},
+            performSignIn: (username: string, password: string) => {
+                dispatch(requestSignIn(username, password))
+            },
             performSignOut: () => dispatch(signOut())
         });
-const SignInForm = connect(mapStateToProps,mapDispatchToProps)(ProtoSignInForm);
+const SignInForm = connect(mapStateToProps, mapDispatchToProps)(ProtoSignInForm);
 
 export default SignInForm
 
